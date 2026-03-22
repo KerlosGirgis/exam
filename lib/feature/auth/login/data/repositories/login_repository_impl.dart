4a6be58bb88db536d013fd10fba6c4a628a40d32
kerlos <kerlos0@protonmail.com>
2026-03-22 21:53:10 +0200

    Refactor storage logic by replacing `LoginLocalDataSource` with a core `SecureStorageService` and a domain-driven `SecureStorageRepository`

--- lib/feature/auth/login/data/repositories/login_repository_impl.dart
+++ lib/feature/auth/login/data/repositories/login_repository_impl.dart
@@ -2,20 +2,22 @@ import 'package:injectable/injectable.dart';
 
 import '../../domain/entities/user_entity.dart';
 import '../../domain/repositories/login_repository.dart';
-import '../datasources/login_local_data_source.dart';
+import '../../domain/repositories/secure_storage_repository.dart';
 import '../datasources/login_remote_data_source.dart';
 
 @Injectable(as: LoginRepository)
 class LoginRepositoryImpl implements LoginRepository {
   final LoginRemoteDataSource remote;
-  final LoginLocalDataSource local;
+  final SecureStorageRepository storage;
 
-  LoginRepositoryImpl(this.remote, this.local);
+  LoginRepositoryImpl(this.remote, this.storage);
 
   @override
   Future<(User, String)> login(String email, String password) async {
     final response = await remote.login(email, password);
-    await local.saveToken(response.token);
+
+    await storage.saveToken(response.token);
+
     return (response.user, response.token);
   }
 }
